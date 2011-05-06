affde8eb7814b8afcb026df4d4bf24b0fa158514
Filipe A Moreira <famoreira@gmail.com>
2011-05-06 09:53:53 +0100

    Adding insert-date function

--- guto/helper.el
+++ guto/helper.el
@@ -55,4 +55,15 @@ table determines which characters these are."
      (list (line-beginning-position)
 	   (line-beginning-position 2)))))
 
+(defun insert-date (prefix)
+    "Insert the current date. With prefix-argument, use ISO format. With
+   two prefix arguments, write out the day and month name."
+    (interactive "P")
+    (let ((format (cond
+                   ((not prefix) "%d.%m.%Y")
+                   ((equal prefix '(4)) "%Y-%m-%d")
+                   ((equal prefix '(16)) "%A, %d. %B %Y")))
+          (system-time-locale "de_DE"))
+      (insert (format-time-string format))))
+
 (provide 'guto/helper)
