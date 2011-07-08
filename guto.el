112990695ce5f39b17cf774454db21301626dfd8
Filipe A Moreira <famoreira@gmail.com>
2011-07-08 10:11:38 +0100

    Backup directory

--- guto.el
+++ guto.el
@@ -124,3 +124,14 @@
   (mapcar (lambda (x) (kill-buffer x))
 	  (buffer-list))
   (delete-other-windows))
+
+;; Backup directory
+
+(setq
+ backup-by-copying t      ; don't clobber symlinks
+ backup-directory-alist
+ '(("." . "~/.saves"))    ; don't litter my fs tree
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+    version-control t)       ; use versioned backups
